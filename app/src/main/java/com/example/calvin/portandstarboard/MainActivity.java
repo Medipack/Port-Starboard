8ffccdb632db0827b2884503b11022f1982d2786
Calvin Chou <ckc21@sfu.ca>
2017-01-22 22:32:47 -0800

    Fixed issue where log would report the wrong side

--- app/src/main/java/com/example/calvin/portandstarboard/MainActivity.java
+++ app/src/main/java/com/example/calvin/portandstarboard/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     //FUNCTIONS
     //Picks a side to be displayed in textView
+    //Accepts a Textview and Game objects as argument
     private void pickASide(TextView question, Game game){
         //takes chosen side and inserts the string into argument for port_or_starboard
         String chosenSide = String.format(getResources().getString(R.string.port_or_starboard), game.getChosenSideName());
@@ -22,12 +23,15 @@ public class MainActivity extends AppCompatActivity {
         question.setText(chosenSide);
     }
 
+    //Purpose:Creates a toast and logs in to logCat
+    //Accepts two string.xml id's, and a string for the side
     public void toastLog(int msg1, int msg2, String side){
         Toast.makeText(getApplicationContext(), msg1, Toast.LENGTH_SHORT).show();
         String logText = String.format(getResources().getString(msg2), side);
         Log.i(TAG, logText);
     }
 
+    //Purpose: overloaded function of toastLog, accepts only 1 string resource and creates both a Toast and log of it
     public void toastLog(int msg1){
         toastLog(msg1, msg1, "");
     }
@@ -40,17 +44,19 @@ public class MainActivity extends AppCompatActivity {
         final TextView quizQuest = (TextView) findViewById(R.id.questionCont);
         //pick a side to start
         pickASide(quizQuest, PortStarboardGame);
-        //wire up left button to log and make toast
-        //...get the button
+
+        //Create left button to show Left
         Button leftbtn = (Button) findViewById(R.id.leftNameBtn);
         //...set up the button to toast when user clicks
         leftbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //log and toast
                 toastLog(R.string.showPort);
             }
         });
 
+        //Create right button to show Right
         Button rightbtn = (Button) findViewById(R.id.rightNameBtn);
         //...set up the button to toast when user clicks
         rightbtn.setOnClickListener(new View.OnClickListener() {
@@ -60,13 +66,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Port button
+        //Left answer button (Port)
         Button leftAnswer = (Button) findViewById(R.id.leftAnsBtn);
         //user clicks one to indicate the game's currently chosen side
         leftAnswer.setOnClickListener(new View.OnClickListener() {
             String gameSide = PortStarboardGame.getChosenSideName();
             @Override
             public void onClick(View v) {
+                String gameSide = PortStarboardGame.getChosenSideName();
                 //checks if the side is PORT
                 if (PortStarboardGame.checkIfCorrect(Game.Side.PORT)){
                     //if yes..
@@ -83,12 +90,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Starboard button
+        //Right answer button (Starboard)
         Button rightAnswer = (Button) findViewById(R.id.rightAnsBtn);
         rightAnswer.setOnClickListener(new View.OnClickListener() {
             String gameSide = PortStarboardGame.getChosenSideName();
             @Override
             public void onClick(View v) {
+                String gameSide = PortStarboardGame.getChosenSideName();
                 //if Side is Starboard,
                 if (PortStarboardGame.checkIfCorrect(Game.Side.STARBOARD)){
                     //Toast and log correct
